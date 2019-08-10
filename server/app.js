fd19efeb9c7d61944741672f75f2a88c9ab22d8e
Mohammadreza Fouladi <m.fouladi@NBSAfzar.Local>
2019-08-10 16:54:04 +0430

    node packages installed

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const helloworldRoutes = require('./routes/helloworld');
+const helloworldRoutes = require('./api/routes/helloworld');
 
 app.use('/helloworld', helloworldRoutes);
 
