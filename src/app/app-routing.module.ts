38e4307175853671fd9d04de0aa44c8a8c918e1b
Mohammadreza Fouladi <m.fouladi@NBSAfzar.Local>
2019-08-10 15:31:46 +0430

    router link added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReportComponent } from './report/report.component';
+import { ProfileComponent } from './users/profile/profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'report', component: ReportComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponenets = [
+  ReportComponent,
+   ProfileComponent,
+    HomeComponent
+  ];
