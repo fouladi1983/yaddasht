a4ac5798ea941c0894b25507e60e7f877d5feb21
Mohammadreza Fouladi <mohammadreza.fouladi@Meditechsys.com>
2019-08-31 16:16:42 +0430

    show user photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { InteractionService } from '@/interaction.service';
-import { AuthServiceService } from '@/auth-service.service';
+import { Component, OnInit } from "@angular/core";
+import { InteractionService } from "@/interaction.service";
+import { AuthServiceService } from "@/auth-service.service";
+import { ShareService } from "./shared/share.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  title = 'yaddasht';
+  title = "yaddasht";
   displaySidebar = true;
-  userEmail = '';
+  userEmail = "";
 
-  constructor(private interactionService: InteractionService, public authService: AuthServiceService) {}
+  userPhoto;
+
+  constructor(
+    private interactionService: InteractionService,
+    public authService: AuthServiceService,
+    private shareService: ShareService
+  ) {}
 
   ngOnInit() {
     this.interactionService.homeMessage$.subscribe(message => {
       if (
-        message === 'homeComponent'
-        || message === 'loginComponent'
-        || message === 'registrationComponent'
-        || message === 'userActivation') {
+        message === "homeComponent" ||
+        message === "loginComponent" ||
+        message === "registrationComponent" ||
+        message === "userActivation"
+      ) {
         this.displaySidebar = false;
       } else {
         this.displaySidebar = true;
       }
     });
+
+    this.shareService
+      .getUserPhoto(localStorage.getItem("userId"))
+      .subscribe(data => {
+        console.log(data);
+        if (data !== null || data == "") {
+          this.userPhoto = data.photo;
+        } else {
+          this.userPhoto = "../assets/img/avatar.png";
+        }
+      });
   }
 
   showSidebar() {
