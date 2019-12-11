600f86d5a56d7f61715a0ab560f7dc57a5f02885
Mohammadreza Fouladi <m.fouladi@WS-05>
2019-12-11 15:11:51 +0330

    add task backend completed

--- server/api/routes/task.js
+++ server/api/routes/task.js
@@ -30,12 +30,12 @@ router.post("/createTask", (req, res) => {
   let projectId = taskInfo.projectId;
 
   sql.close();
-  sql.connect(config, (err) => {
+  sql.connect(config, err => {
     if (err)
       res.status(503).json({
         message: "خطایی در اتصال به دیتابیس رخ داده است لطفا دوباره تلاش نمایید"
       });
-     let request = new sql.Request();
+    let request = new sql.Request();
     request.query(
       `insert into taskInfo(name,description,startDate,endDate)
                     values('${taskInfo.name}','${taskInfo.description}','${taskInfo.startDate}','${taskInfo.endDate}')`,
@@ -45,59 +45,72 @@ router.post("/createTask", (req, res) => {
           res.status(503).json({
             message:
               "خطایی در ثبت اطلاعات کاربر رخ داده است لطفا دوباره تلاش نمایید"
-          });}
+          });
           sql.close();
-        // } else {
-        //   request.query(
-        //     `select id as lastID from taskInfo where id = @@Identity`,
-        //     (err, result) => {
-        //       if (err) {
-        //         res.status(503).json({
-        //           message:
-        //             "خطایی در ثبت اطلاعات کاربر رخ داده است لطفا دوباره تلاش نمایید"
-        //         });
-        //         sql.close();
-        //       } else {
-        //         task.taskInfoId = result.recordset[0].id;
-        //         task.direct = taskInfo.direct;
-        //         task.userId = taskInfo.userId;
+        } else {
+          request.query(
+            `select id as lastID from taskInfo where id = @@Identity`,
+            (err, result) => {
+              // res.status(200).json({
+              //   message: result.recordset[0].lastID});
+              if (err) {
+                res.status(503).json({
+                  message:
+                    "خطایی در ثبت اطلاعات کاربر رخ داده است لطفا دوباره تلاش نمایید"
+                });
+                sql.close();
+                //OK
+              } else {
+                task.taskInfoId = result.recordset[0].lastID;
+                if (taskInfo.direct === true) {
+                  task.direct = 1;
+                } else {
+                  task.direct = 0;
+                }
+                task.userId = taskInfo.userId;
 
-        //         request.query(
-        //           `insert into task(taskInfoId,userId,direct)
-        //                                     values('${task.taskInfoId}','${task.direct}','${task.userId}')`,
-        //           (err, result) => {
-        //             if (err) {
-        //               res.status(503).json({
-        //                 message:
-        //                   "خطایی در ثبت اطلاعات کاربر رخ داده است لطفا دوباره تلاش نمایید"
-        //               });
-        //               sql.close();
-        //             } else {
-        //               let taskId = result.recordset[0].id;
-        //               request.query(
-        //                 `insert into projectTasks(taskId,projectId)
-        //                               values('${taskId}','${projectId}')`,
-        //                 (err, result) => {
-        //                   if (err) {
-        //                     res.status(503).json({
-        //                       message:
-        //                         "خطایی در ثبت اطلاعات کاربر رخ داده است لطفا دوباره تلاش نمایید"
-        //                     });
-        //                     sql.close();
-        //                   } else {
-        //                     res.status(200).json({
-        //                       message: `با موفقیت ثبت گردید ${taskInfo.name}`
-        //                     });
-        //                   }
-        //                 }
-        //               );
-        //             }
-        //           }
-        //         );
-        //       }
-        //     }
-        //   );
-        // }
+                request.query(
+                  `insert into tasks(taskInfoId,userId,direct)
+                                            values('${task.taskInfoId}','${task.direct}','${task.userId}')`,
+                  (err, result) => {
+                    console.log(task.direct, task.taskInfoId, task.userId);
+                    if (err) {
+                      res.status(503).json({
+                        message:
+                          "خطایی در ثبت اطلاعات کاربر رخ داده است لطفا دوباره تلاش نمایید"
+                      });
+                      sql.close();
+                    } else {
+                      request.query(
+                        `select id from tasks where id = @@Identity`,
+                        (err, result) => {
+                          let taskId = result.recordset[0].id;
+                          request.query(
+                            `insert into projectTasks(taskId,projectId)
+                                             values('${taskId}','${projectId}')`,
+                            (err, result) => {
+                              if (err) {
+                                res.status(503).json({
+                                  message:
+                                    "خطایی در ثبت اطلاعات کاربر رخ داده است لطفا دوباره تلاش نمایید"
+                                });
+                                sql.close();
+                              } else {
+                                res.status(200).json({
+                                  message: `با موفقیت ثبت گردید ${taskInfo.name}`
+                                });
+                              }
+                            }
+                          );
+                        }
+                      );
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
       }
     );
   });
