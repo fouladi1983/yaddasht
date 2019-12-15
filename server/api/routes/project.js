eb800fcf9013b311d8fb651d3542ca2f3cb28372
Mohammadreza Fouladi <m.fouladi@WS-05>
2019-12-15 08:18:24 +0330

    feat: get project names

--- server/api/routes/project.js
+++ server/api/routes/project.js
@@ -10,6 +10,15 @@ router.get('/', (req,res) => {
     if(err) res.status(503).json({ message: 'خطایی در اتصال به دیتابیس رخ داده است لطفا دوباره تلاش نمایید'});
 
     const request = new sql.Request();
+    request.query(`select [pi].[name], [pi].startDate, [pi].endDate from projects p join projectInfo [pi]
+                    on p.projectInfoId = [pi].id
+                    where p.userId = ${userId}`, (err,result) => {
+                      if(err) res.status(503).json({ message: 'خطایی در اتصال به دیتابیس رخ داده است لطفا دوباره تلاش نمایید'});
+
+                      res.status(200).json({
+                        result: result.recordset
+                      })
+                    });
   });
 })
 
